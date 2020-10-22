cc936fcf927520e7b621d7b349151a620072a395
maikonFoltz <maikonjfoltz@gmail.com>
2020-10-21 21:56:39 -0300

    ok calender

--- calendario.php
+++ calendario.php
@@ -1,13 +1,42 @@
+<form action="" method="get">
+    <select name="ano" id="ano">
+        <?php for($q=date('Y'); $q>2010; $q--):?>
+            <option value="<?php echo $q;?>"><?php echo $q;?></option>
+        <?php endfor;?>    
+    </select>
+    <select name="mes" id="mes">
+        <option value="1">01</option>
+        <option value="2">02</option>
+        <option value="3">03</option>
+        <option value="4">04</option>
+        <option value="5">05</option>
+        <option value="6">06</option>
+        <option value="7">07</option>
+        <option value="8">08</option>
+        <option value="9">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        
+    </select>
+
+    <input type="submit" value="Mostrar">
+</form>
+
 <?php
-    $data = '2020-01';
+    if(empty($_GET['ano'])){
+        exit;
+    }
+    $data = $_GET['ano'].' - '.$_GET['mes'];
     $dia1 = date('w', strtotime($data));
     $quantidadeDiasMes = date('t', strtotime($data));
     $quantidadeLinhasCalendario = ceil(($dia1 + $quantidadeDiasMes) / 7);   
     $dia1 = -$dia1;
     $dataInicio = date('Y-m-d', strtotime($dia1.' days',strtotime($data)));
     $dataFim = date('Y-m-d', strtotime((($dia1 + ($quantidadeLinhasCalendario * 7) -1)).' days',strtotime($data)));
-    echo $dataFim;
+    
 ?>
+
 
 <table border="1" width="100%">
     <tr>
